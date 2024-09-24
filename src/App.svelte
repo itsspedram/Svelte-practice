2b08752d4f2dc2aaeb3f8146e719b0763de198e8
itsspedram <pedmsv@gmail.com>
2024-09-24 16:35:33 +0330

    add remover

--- src/App.svelte
+++ src/App.svelte
@@ -5,6 +5,9 @@ let people = [
     { name: 'mario', beltColour: 'orange', age: 45, id: 2 },
     { name: 'luigi', beltColour: 'brown', age: 35, id: 3 }
   ];
+  const handleClick = (id)=>{
+    people = people.filter((person)=>person.id !=id )
+  }
 
 </script>
 
@@ -12,6 +15,7 @@ let people = [
 {#each people as person (person.id) }
   <h3>{person.name}</h3>
   <h4>{person.age} years old with {person.beltColour} belt</h4>
+  <button on:click={()=>handleClick(person.id)}>delete </button>
   {:else}
   <h1>its empty :))</h1>
 {/each}
