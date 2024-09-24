db4cbcc753200714be5293ee6bed2bdcfaf5d615
itsspedram <pedmsv@gmail.com>
2024-09-24 16:03:14 +0330

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,32 @@
+<script>
+
+  let name = "esi"
+  let counter = 0
+
+  const countUp = ()=>{
+    counter++;
+  }
+</script>
+
+<main>
+<h1>Hello my name is {name}</h1>
+<input type="text" bind:value={name}>
+</main>
+
+<style>
+  .logo {
+    height: 6em;
+    padding: 1.5em;
+    will-change: filter;
+    transition: filter 300ms;
+  }
+  .logo:hover {
+    filter: drop-shadow(0 0 2em #646cffaa);
+  }
+  .logo.svelte:hover {
+    filter: drop-shadow(0 0 2em #ff3e00aa);
+  }
+  .read-the-docs {
+    color: #888;
+  }
+</style>
---
+++ src/main.js
@@ -0,0 +1,11 @@
+import './app.css'
+import App from './App.svelte'
+
+const app = new App({
+  target: document.getElementById('app'),
+  props:{
+    duuuuude:"IM a test "
+  }
+})
+
+export default app
